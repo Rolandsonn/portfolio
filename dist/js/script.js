17fe020bbeebc001d32bbda68a001d211c31902a
Ruslan <stamkulovruslan72@gmail.com>
2022-09-23 12:19:22 +0600

    start-project

---
+++ dist/js/script.js
@@ -0,0 +1,28 @@
+// // const mainBtn = document.querySelector("#main_button");
+// const mainBtn = document.getElementById("main_button");
+// const backDrop = document.getElementById("backdrop");
+// const modal = document.querySelector(".modal");
+// const btnClose = document.getElementById("btn-close");
+// // console.log(document.body.);
+
+// // const div = document.createElement("div");
+// // div.innerHTML = "<span>Hello</span> World <ul> <li>Это Li</li></ul>";
+// // div.className = "wrapper";
+// // console.log(div);
+
+// // document.body.append(div);
+// // setTimeout(() => div.remove(), 3000);
+// mainBtn.addEventListener("click", () => {
+//   modaltoggler(backDrop);
+// });
+// backDrop.addEventListener("click", (e) => {
+//   if (e.target === e.currentTarget) {
+//     modaltoggler(backDrop);
+//   }
+// });
+// btnClose.addEventListener("click", () => {
+//   modaltoggler(backDrop);
+// });
+// function modaltoggler(el) {
+//   el.classList.toggle("hidden");
+// }
